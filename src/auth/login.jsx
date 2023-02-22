30362676edb7593f4a4484bda3ffea8aa4608e79
Idris Yakub <olayinkayakub@yahoo.com>
2023-02-23 00:49:32 +0100

    passing user down the app

--- src/auth/login.jsx
+++ src/auth/login.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../utils/auth';
 
 const Login = () => {
   const [user, setUser] =  useState("");
+  const [email, setEmail] =  useState("");
+  const [pwd, setPwd] =  useState("");
   const auth = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
@@ -11,14 +13,21 @@ const Login = () => {
   const redirectPath = location.state?.path || '/'
 
   const handleLogin = (e) => {
+    const {email, pwd} = e.target.elements;
+    console.log('data', {email: email.value, pwd: pwd.value});
     auth.login(user)
     navigate(redirectPath, { replace: true })
   }
 
+  useEffect(()=>{
+    setUser("Idris");
+  },[])
+
   return (
-    <form action="" >
-      <input type="text" name='user' value={user} placeholder='enter ur name' onChange={(e)=>setUser(e.target.value)} required />
-      <button type="submit" onClick={handleLogin}>Submit</button>
+    <form action="" onSubmit={handleLogin} >
+      <input type="email" name='email' value={email} placeholder='enter ur email address' onChange={(e)=>setEmail(e.target.value)} />
+      <input type="password" name='pwd' value={pwd} placeholder='enter ur password' onChange={(e)=>setPwd(e.target.value)} />
+      <button type="submit">Submit</button>
     </form>
   )
 }
